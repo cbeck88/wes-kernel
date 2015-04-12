3123d3bde29d6ff962a5b11ea499c621245dcd2d
Chris Beck <render787@gmail.com>
2015-04-12 13:57:02 -0400

    run clang-format on wml_parser.cpp
    clang-format-3.5 wml_parser.cpp -style=file > temp
    mv temp wml_parser.cpp
    rm temp

--- src/wml_parser.cpp
+++ src/wml_parser.cpp
@@ -22,184 +22,168 @@
 
 namespace wml
 {
-    namespace fusion = boost::fusion;
-    namespace phoenix = boost::phoenix;
-    namespace qi = boost::spirit::qi;
-    namespace ascii = boost::spirit::ascii;
+	namespace fusion = boost::fusion;
+	namespace phoenix = boost::phoenix;
+	namespace qi = boost::spirit::qi;
+	namespace ascii = boost::spirit::ascii;
 
-    ///////////////////////////////////////////////////////////////////////////
-    //  Our WML tree representation
-    ///////////////////////////////////////////////////////////////////////////
-    struct body;
+	///////////////////////////////////////////////////////////////////////////
+	//  Our WML tree representation
+	///////////////////////////////////////////////////////////////////////////
+	struct body;
 
-    typedef
-        boost::variant<
-            boost::recursive_wrapper<body>
-          , std::string
-        >
-    node;
+	typedef boost::variant<boost::recursive_wrapper<body>, std::string>
+		node;
 
-    struct body
-    {
-        std::string name;                      // tag name
-        std::vector<node> children;        // children
-    };
+	struct body
+	{
+		std::string name;	   // tag name
+		std::vector<node> children; // children
+	};
 }
 
 // We need to tell fusion about our mini_xml struct
 // to make it a first-class fusion citizen
 BOOST_FUSION_ADAPT_STRUCT(
-    wml::body,
-    (std::string, name)
-    (std::vector<wml::node>, children)
-)
+	wml::body,
+	(std::string, name)(std::vector<wml::node>, children))
 
 namespace wml
 {
-    ///////////////////////////////////////////////////////////////////////////
-    //  Print out the mini xml tree
-    ///////////////////////////////////////////////////////////////////////////
-    int const tabsize = 4;
+	///////////////////////////////////////////////////////////////////////////
+	//  Print out the mini xml tree
+	///////////////////////////////////////////////////////////////////////////
+	int const tabsize = 4;
 
-    void tab(int indent)
-    {
-        for (int i = 0; i < indent; ++i)
-            std::cout << ' ';
-    }
+	void tab(int indent)
+	{
+		for(int i = 0; i < indent; ++i)
+			std::cout << ' ';
+	}
 
-    struct body_printer
-    {
-        body_printer(int indent = 0)
-          : indent(indent)
-        {
-        }
+	struct body_printer
+	{
+		body_printer(int indent = 0)
+		    : indent(indent)
+		{
+		}
 
-        void operator()(body const&) const;
+		void operator()(body const&) const;
 
-        int indent;
-    };
+		int indent;
+	};
 
-    struct node_printer : boost::static_visitor<>
-    {
-        node_printer(int indent = 0)
-          : indent(indent)
-        {
-        }
+	struct node_printer : boost::static_visitor<>
+	{
+		node_printer(int indent = 0)
+		    : indent(indent)
+		{
+		}
 
-        void operator()(body const& w) const
-        {
-            body_printer(indent+tabsize)(w);
-        }
+		void operator()(body const& w) const
+		{
+			body_printer(indent + tabsize)(w);
+		}
 
-        void operator()(std::string const& text) const
-        {
-            tab(indent+tabsize);
-            std::cout << "text: \"" << text << '"' << std::endl;
-        }
+		void operator()(std::string const& text) const
+		{
+			tab(indent + tabsize);
+			std::cout << "text: \"" << text << '"' << std::endl;
+		}
 
-        int indent;
-    };
+		int indent;
+	};
 
-    void body_printer::operator()(body const& w) const
-    {
-        tab(indent);
-        std::cout << "tag: '" << w.name << "'" << std::endl;
-        tab(indent);
-        std::cout << '{' << std::endl;
+	void body_printer::operator()(body const& w) const
+	{
+		tab(indent);
+		std::cout << "tag: '" << w.name << "'" << std::endl;
+		tab(indent);
+		std::cout << '{' << std::endl;
 
-        BOOST_FOREACH(node const& n, w.children)
-        {
-            boost::apply_visitor(node_printer(indent), n);
-        }
+		BOOST_FOREACH(node const& n, w.children) {
+			boost::apply_visitor(node_printer(indent), n);
+		}
 
-        tab(indent);
-        std::cout << '}' << std::endl;
-    }
+		tab(indent);
+		std::cout << '}' << std::endl;
+	}
 
-    ///////////////////////////////////////////////////////////////////////////
-    //  Our mini XML grammar definition
-    ///////////////////////////////////////////////////////////////////////////
-    //[tutorial_xml2_grammar
-    template <typename Iterator>
-    struct wml_grammar
-      : qi::grammar<Iterator, body(), qi::locals<std::string>, ascii::space_type>
-    {
-        wml_grammar()
-          : wml_grammar::base_type(wml)
-        {
-            using qi::lit;
-            using qi::lexeme;
-            using ascii::char_;
-            using ascii::string;
-            using namespace qi::labels;
+	///////////////////////////////////////////////////////////////////////////
+	//  Our mini XML grammar definition
+	///////////////////////////////////////////////////////////////////////////
+	//[tutorial_xml2_grammar
+	template <typename Iterator>
+	struct wml_grammar
+		: qi::grammar<Iterator, body(), qi::locals<std::string>, ascii::space_type>
+	{
+		wml_grammar()
+		    : wml_grammar::base_type(wml)
+		{
+			using qi::lit;
+			using qi::lexeme;
+			using ascii::char_;
+			using ascii::string;
+			using namespace qi::labels;
 
-            text %= lexeme[+(char_ - '[')];
-            node %= wml | text;
+			text %= lexeme[+(char_ - '[')];
+			node %= wml | text;
 
-            start_tag %=
-                    '['
-                >>  !lit('/')
-                >>  lexeme[+(char_ - ']')]
-                >>  ']'
-            ;
+			start_tag %= '['
+				     >> !lit('/')
+				     >> lexeme[+(char_ - ']')]
+				     >> ']';
 
-            end_tag =
-                    "[/"
-                >>  string(_r1)
-                >>  ']'
-            ;
+			end_tag = "[/"
+				  >> string(_r1)
+				  >> ']';
 
-            wml %=
-                    start_tag[_a = _1]
-                >>  *node
-                >>  end_tag(_a)
-            ;
-        }
+			wml %= start_tag[_a = _1]
+			       >> *node
+			       >> end_tag(_a);
+		}
 
-        qi::rule<Iterator, wml::body(), qi::locals<std::string>, ascii::space_type> wml;
-        qi::rule<Iterator, wml::node(), ascii::space_type> node;
-        qi::rule<Iterator, std::string(), ascii::space_type> text;
-        qi::rule<Iterator, std::string(), ascii::space_type> start_tag;
-        qi::rule<Iterator, void(std::string), ascii::space_type> end_tag;
-    };
-    //]
+		qi::rule<Iterator, wml::body(), qi::locals<std::string>, ascii::space_type> wml;
+		qi::rule<Iterator, wml::node(), ascii::space_type> node;
+		qi::rule<Iterator, std::string(), ascii::space_type> text;
+		qi::rule<Iterator, std::string(), ascii::space_type> start_tag;
+		qi::rule<Iterator, void(std::string), ascii::space_type> end_tag;
+	};
+	//]
 }
 
 ///////////////////////////////////////////////////////////////////////////////
 //  Main program
 ///////////////////////////////////////////////////////////////////////////////
-namespace wml {
-
-bool parse(const std::string & storage)
+namespace wml
 {
-    typedef wml_grammar<std::string::const_iterator> my_grammar;
-    my_grammar xml; // Our grammar
-    wml::body ast; // Our tree
 
-    using boost::spirit::ascii::space;
-    std::string::const_iterator iter = storage.begin();
-    std::string::const_iterator end = storage.end();
-    bool r = phrase_parse(iter, end, xml, space, ast);
+	bool parse(const std::string& storage)
+	{
+		typedef wml_grammar<std::string::const_iterator> my_grammar;
+		my_grammar xml; // Our grammar
+		wml::body ast;  // Our tree
 
-    if (r && iter == end)
-    {
-        std::cout << "-------------------------\n";
-        std::cout << "Parsing succeeded\n";
-        std::cout << "-------------------------\n";
-        body_printer printer;
-        printer(ast);
-        return true;
-    }
-    else
-    {
-        std::string::const_iterator some = iter+30;
-        std::string context(iter, (some>end)?end:some);
-        std::cout << "-------------------------\n";
-        std::cout << "Parsing failed\n";
-        std::cout << "stopped at: \": " << context << "...\"\n";
-        std::cout << "-------------------------\n";
-        return false;
-    }
-}
+		using boost::spirit::ascii::space;
+		std::string::const_iterator iter = storage.begin();
+		std::string::const_iterator end = storage.end();
+		bool r = phrase_parse(iter, end, xml, space, ast);
 
+		if(r && iter == end) {
+			std::cout << "-------------------------\n";
+			std::cout << "Parsing succeeded\n";
+			std::cout << "-------------------------\n";
+			body_printer printer;
+			printer(ast);
+			return true;
+		} else {
+			std::string::const_iterator some = iter + 30;
+			std::string context(iter, (some > end) ? end : some);
+			std::cout << "-------------------------\n";
+			std::cout << "Parsing failed\n";
+			std::cout << "stopped at: \": " << context << "...\"\n";
+			std::cout << "-------------------------\n";
+			return false;
+		}
+	}
 }
