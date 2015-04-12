0df87394edb41da551dc3613d86fc63ecb4b9b27
Chris Beck <render787@gmail.com>
2015-04-12 13:00:13 -0400

    split off json parser and key value sequence executable targets

--- src/wesnoth.cpp
+++ src/wesnoth.cpp
@@ -1,25 +1,3 @@
-
-/*
-#include "parser.hpp"
-
-#include <boost/property_tree/ptree.hpp>
-#include <boost/property_tree/xml_parser.hpp>
-
-using boost::property_tree::ptree;
-
-#include <string>
-#include <sstream>
-
-int main() {
-	std::string test_doc = "<root>Some text<bold attr='blah' attr2='bleh'>more text</bold>yet more text</root>";
-	std::istringstream s(test_doc);
-	ptree pt;
-	boost::property_tree::xml_parser::read_xml(s, pt, boost::property_tree::xml_parser::no_concat_text);
-
-	handle_node(pt, 0);
-}
-*/
-
 #include "wml_parser.hpp"
 
 #include <fstream>
