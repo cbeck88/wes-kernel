a3114cc6ad8135094f82705b4a46aa573d22bf70
Chris Beck <render787@gmail.com>
2015-04-12 06:28:34 -0400

    add `using boost::property_tree::ptree`

--- src/parser.cpp
+++ src/parser.cpp
@@ -4,9 +4,11 @@
 
 #include <string>
 
-void handle_node(const boost::property_tree::ptree& ptree, int depth)
+using boost::property_tree::ptree;
+
+void handle_node(const ptree& pt, int depth)
 {
-	for(auto itor = ptree.begin(); itor != ptree.end(); ++itor) {
+	for(auto itor = pt.begin(); itor != pt.end(); ++itor) {
 		for(int n = 0; n != depth; ++n) { std::cerr << "  "; }
 		std::cerr << "XML: " << itor->first << " -> " << itor->second.data() << "\n";
 		handle_node(itor->second, depth+1);
