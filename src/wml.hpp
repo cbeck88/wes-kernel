c0c297b2bf43536a92feddd670f6bdbb536e97d7
Chris Beck <render787@gmail.com>
2015-04-16 16:39:17 -0400

    move wml defintiion outside of wml parser

---
+++ src/wml.hpp
@@ -0,0 +1,75 @@
+#pragma once
+
+#include <string>
+#include <utility>
+#include <vector>
+#include <boost/variant/recursive_variant.hpp>
+#include <boost/foreach.hpp>
+
+namespace wml {
+
+typedef std::string Str;
+typedef std::pair<Str, Str> Pair;
+
+///////////////////////////////////////////////////////////////////////////
+//  Our WML tree representation
+///////////////////////////////////////////////////////////////////////////
+struct body;
+
+typedef boost::variant<boost::recursive_wrapper<body>, Pair> node;
+
+struct body {
+	Str name;                   // tag name
+	std::vector<node> children; // children
+};
+
+///////////////////////////////////////////////////////////////////////////
+//  Print out the wml tree
+///////////////////////////////////////////////////////////////////////////
+int const tabsize = 4;
+
+void tab(int indent) {
+	for (int i = 0; i < indent; ++i)
+		std::cout << ' ';
+}
+
+struct body_printer {
+	body_printer(int indent = 0) : indent(indent) {}
+
+	void operator()(body const&) const;
+
+	int indent;
+};
+
+struct node_printer : boost::static_visitor<> {
+	node_printer(int indent = 0) : indent(indent) {}
+
+	void operator()(body const& w) const { body_printer(indent + tabsize)(w); }
+
+	void operator()(Str const& text) const {
+		tab(indent + tabsize);
+		std::cout << "text: \"" << text << '"' << std::endl;
+	}
+
+	void operator()(Pair const& p) const {
+		tab(indent + tabsize);
+		std::cout << p.first << ": \"" << p.second << "\"" << std::endl;
+	}
+
+	int indent;
+};
+
+void body_printer::operator()(body const& w) const {
+	tab(indent);
+	std::cout << "tag: \"" << w.name << "\"" << std::endl;
+	tab(indent);
+	std::cout << '{' << std::endl;
+
+	BOOST_FOREACH (node const& n, w.children) { boost::apply_visitor(node_printer(indent), n); }
+
+	tab(indent);
+	std::cout << '}' << std::endl;
+}
+
+
+} // end namespace wml
--- src/wml_parser.cpp
+++ src/wml_parser.cpp
@@ -6,6 +6,8 @@
 #define BOOST_SPIRIT_USE_PHOENIX_V3
 #define BOOST_SPIRIT_UNICODE
 
+#include "wml.hpp"
+
 #include <boost/config/warning_disable.hpp>
 #include <boost/spirit/include/qi.hpp>
 #include <boost/spirit/include/phoenix_core.hpp>
@@ -21,81 +23,17 @@
 #include <iostream>
 #include <fstream>
 #include <string>
+#include <utility>
 #include <vector>
-
-namespace wml {
-namespace fusion = boost::fusion;
-namespace phoenix = boost::phoenix;
-namespace qi = boost::spirit::qi;
-
-typedef std::string Str;
-typedef std::pair<Str, Str> Pair;
-
-///////////////////////////////////////////////////////////////////////////
-//  Our WML tree representation
-///////////////////////////////////////////////////////////////////////////
-struct body;
-
-typedef boost::variant<boost::recursive_wrapper<body>, Pair> node;
-
-struct body {
-	Str name;                   // tag name
-	std::vector<node> children; // children
-};
-} // end namespace wml
 
 // We need to tell fusion about our wml struct
 // to make it a first-class fusion citizen
 BOOST_FUSION_ADAPT_STRUCT(wml::body, (wml::Str, name)(std::vector<wml::node>, children))
 
 namespace wml {
-///////////////////////////////////////////////////////////////////////////
-//  Print out the wml tree
-///////////////////////////////////////////////////////////////////////////
-int const tabsize = 4;
-
-void tab(int indent) {
-	for (int i = 0; i < indent; ++i)
-		std::cout << ' ';
-}
-
-struct body_printer {
-	body_printer(int indent = 0) : indent(indent) {}
-
-	void operator()(body const&) const;
-
-	int indent;
-};
-
-struct node_printer : boost::static_visitor<> {
-	node_printer(int indent = 0) : indent(indent) {}
-
-	void operator()(body const& w) const { body_printer(indent + tabsize)(w); }
-
-	void operator()(Str const& text) const {
-		tab(indent + tabsize);
-		std::cout << "text: \"" << text << '"' << std::endl;
-	}
-
-	void operator()(Pair const& p) const {
-		tab(indent + tabsize);
-		std::cout << p.first << ": \"" << p.second << "\"" << std::endl;
-	}
-
-	int indent;
-};
-
-void body_printer::operator()(body const& w) const {
-	tab(indent);
-	std::cout << "tag: \"" << w.name << "\"" << std::endl;
-	tab(indent);
-	std::cout << '{' << std::endl;
-
-	BOOST_FOREACH (node const& n, w.children) { boost::apply_visitor(node_printer(indent), n); }
-
-	tab(indent);
-	std::cout << '}' << std::endl;
-}
+namespace fusion = boost::fusion;
+namespace phoenix = boost::phoenix;
+namespace qi = boost::spirit::qi;
 
 ///////////////////////////////////////////////////////////////////////////
 //  Our WML grammar definition
